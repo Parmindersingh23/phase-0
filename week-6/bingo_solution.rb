27fd8e1c7db16b9fc012a89c8cf1e71a7da25e57
Parminder Singh <psing006@ucr.edu>
2015-11-09 10:48:54 -0600

    Bingo

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -35,6 +35,8 @@
 
 =begin
 
+#my intial solution didnt really work or was not complete. So I tried to do it in a completley different way and struggled through it!!
+
 class BingoBoard
 
   def initialize(board)
@@ -68,32 +70,39 @@ class BingoBoard
 
 =end
 
+#Refactored Code
+
 class BingoBoard
 
   def initialize(board)
-    @bingo_board = board
+    @bingo_board = Array.new(5){Array(5.times.map{rand(1..100)})}
   end
 
   def call
-
-    @letter = ['B','I','N','G','O'].sample
-    @number = rand(1..100)
-
+    @column = rand(5)
+    @random_numbers = rand(1..100)
   end
 
   def check
-    call
-    @bingo_board.each do |n|
-      index = n.index(@number)
-      n[index] = 'X' unless index.nil?
-    end
-
+    @bingo_board.each{|row| if row [@column] == @random_numbers
+      row[@column] = "XX" end}
   end
 
+  def display_board
+    puts "B   I   N   G   O!"
+  20.times do
+  print "*"
+  sleep 0.1
+  end
+  puts "  "
+  @bingo_board.each { |element| puts "   " + element.join("  ").to_s }
+  20.times do
+  print "*"
+  sleep 0.1
+  end
+  p
+  end
 end
-
-
-
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 board = [[47, 44, 71, 8, 88],
@@ -103,9 +112,43 @@ board = [[47, 44, 71, 8, 88],
         [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
-new_game.number_letter
-new_game.checker
+new_game.call
+new_game.check
+new_game.display_board
 
+=begin
 #Reflection
 
+How difficult was pseudocoding this challenge? What do you think of your pseudocoding style?
 
+I don't think the psuedocoding was the hard part, but actually translating it into code was hard for me. I made a lot of changes in code that I didnt have in my pseudocode.
+
+I think I am slowly getting better at it but sometimes it is still hard to clearly write out everything.
+What are the benefits of using a class for this challenge?
+
+The benefits of instance variables are helpful here because we are accessing many different things. Using a class helps by letting all the methods access whatever they need withough any conflict.
+
+
+How can you access coordinates in a nested array?
+
+You can access corrdinates by the bracket method such as : array[row] or array[column]
+
+What methods did you use to access and modify the array?
+
+I used .each to go over the array and also .map.
+
+Give an example of a new method you learned while reviewing the Ruby docs. Based on
+what you see in the docs, what purpose does it serve, and how is it called?
+
+.map can be used to create a new array based on the original array and in fast way.
+
+How did you determine what should be an instance variable versus a local variable?
+
+When ever I needed to call anything outside of each method within another method I knew which ones do use. I didnt focus on using local variables in this challenge since it was not the focus.
+
+What do you feel is most improved in your refactored solution?
+
+Making the Bingo game actually work! It was a tough challenge and I made alot of changes through out and got rid of alot of code that I didn't even need. I would just comment out lines and then delete them if that piece did not break the code.
+
+
+=end
