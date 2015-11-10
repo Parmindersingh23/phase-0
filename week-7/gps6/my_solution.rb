ede64d15ab20f37f46224736c26ed14d8dd3d3e2
Parminder Singh <psing006@ucr.edu>
2015-11-09 18:42:05 -0600

    full solution

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -1,18 +1,89 @@
+# Virus Predictor
+
+# Nil Thacker and Parminder Singh worked on this challenge.
+# We spent 1 hours on this challenge.
+
+# EXPLANATION OF require_relative
+#What does it do?
+# It includes the contents from another file whose path is relative to the current file.
+
+# How does it work?
+# It includes the contents of the other file where the require_relative command is called.
+
+# How does it differ from require?
+# require is most commonly used to import library files
+
+require_relative 'state_data'
+
+#population density is number of people per square mile as of 2012
+#this data is updated every year with estimates from a 10 year census
+class VirusPredictor
+
+  #We are creating instance variables for the initialized instance
+  def initialize(state_of_origin, population_density, population)
+    @state = state_of_origin
+    @population = population
+    @population_density = population_density
+  end
+
+  #Call 2 other functions and pass them instance variables
+
+  def virus_effects
+    puts "#{@state} will lose #{predicted_deaths} people in this outbreak and will spread across the state in #{speed_of_spread} months.\n\n"
+  end
 
 
-=begin
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+  private
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+  #Depending on the Population density, we calulate the number of predicted deaths
+  def predicted_deaths
+    case @population_density
+    when 0..50    then (@population * 0.05).floor
+    when 51..100  then (@population * 0.1).floor
+    when 101..150 then (@population * 0.2).floor
+    when 151..200 then (@population * 0.3).floor
+    else (@population * 0.4).floor
+    end
+  end
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+  #Calculate how long the virus will take to spread based on pop density
+  def speed_of_spread #in months
+    # We are still perfecting our formula here. The speed is also affected
+    # by additional factors we haven't added into this functionality.
+    case @population_density
+    when 0..50    then 2.5
+    when 51..100  then 2
+    when 101..150 then 1.5
+    when 151..200 then 1
+    else 0.5
+    end
+  end
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-=end
+end
+
+#=======================================================================
+
+# DRIVER CODE
+ # initialize VirusPredictor for each state
+
+STATE_DATA.each do |state, population_data|
+  VirusPredictor.new(state, population_data[:population_density], population_data[:population]).virus_effects
+end
+
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
+
+
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
+
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
+
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+
 
 #=======================================================================
 # Reflection Section
